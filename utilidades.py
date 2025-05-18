b23d1eca20e2f3a1a3e50f2f6bbf2a9d4650c5f6
Karen2878 <sofiruedapin@gmail.com>
2025-05-17 19:34:41 -0500

    Resuelto conflicto manual en utilidades.py

--- utilidades.py
+++ utilidades.py
@@ -1,34 +1,20 @@
-<<<<<<< HEAD
 
 def sumar(a, b):
- return a + b
-def restar(a, b):
- return a - b
-def multiplicar(a, b):
- return a * b
-def dividir(a, b):
- if b == 0:
-    return "Error: división por cero"
- return a / b
-def operacion_innecesaria(x):
- if x > 0:
-    return True
- else:
-=======
+    return a + b
 
-def sumar(a, b):
- return a + b
 def restar(a, b):
- return a - b
+    return a - b
+
 def multiplicar(a, b):
- return a * b
+    return a * b
+
 def dividir(a, b):
- if b == 0:
-    return "Error: división por cero"
- return a / b
+    if b == 0:
+        return "Error: división por cero"
+    return a / b
+
 def operacion_innecesaria(x):
- if x > 0:
-    return True
- else:
->>>>>>> 56b94b347aff399b9da498042d4b2c619526cff5
-    return True
+    if x > 0:
+        return True
+    else:
+        return True
